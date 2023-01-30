e45950aff9a76c3aa14d8ab2a89f1e25fed049cc
younngg <8533283@naver.com>
2023-01-30 23:44:12 +0900

    fix: index 외부링크 새창열기 수정

--- pages/index.tsx
+++ pages/index.tsx
@@ -18,17 +18,29 @@ export default function Home() {
           data-aos-duration='3000'
         >
           <p>
-            <a href='https://github.com/Younngg/'>
+            <a
+              href='https://github.com/Younngg/'
+              target='_blank'
+              rel='noreferrer'
+            >
               <strong>Github</strong>
             </a>
           </p>
           <p>
-            <a href='https://velog.io/@younngg1012'>
+            <a
+              href='https://velog.io/@younngg1012'
+              target='_blank'
+              rel='noreferrer'
+            >
               <strong>Velog</strong>
             </a>
           </p>
           <p>
-            <a href='https://my.surfit.io/w/1501176912'>
+            <a
+              href='https://my.surfit.io/w/1501176912'
+              target='_blank'
+              rel='noreferrer'
+            >
               <strong>Resume</strong>
             </a>
           </p>
