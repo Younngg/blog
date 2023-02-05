66d14e1626f586a6775867eebaf5fa35de88f626
younngg <8533283@naver.com>
2023-02-05 21:05:12 +0900

    fix: daily page 오류 수정

--- components/DailyItem/DailyItem.tsx
+++ components/DailyItem/DailyItem.tsx
@@ -11,20 +11,17 @@ interface DailyItemProps {
 }
 
 const DailyItem = ({ date, title, des, id, tag }: DailyItemProps) => {
+  console.log(id);
   return (
     <Link href={`/daily/${id}`} passHref>
       <div className='w-full h-80 px-12 py-10 mb-5 border border-gray-200 shadow-md flex flex-col justify-between gap-5 hover:-translate-y-2 hover:shadow-lg transition-all'>
         <div className='flex flex-col gap-5'>
-          <a href='' target='_blank' rel='noreferrer'>
-            <h3 className='font-semibold text-neutral-700 mb-1 text-xl'>
-              {title}
-            </h3>
-          </a>
-          <a href='' target='_blank' rel='noreferrer'>
-            <p className='w-full break-all text-ellipsis line-clamp-4 text-neutral-600 text-sm leading-7'>
-              {des}
-            </p>
-          </a>
+          <h3 className='font-semibold text-neutral-700 mb-1 text-xl'>
+            {title}
+          </h3>
+          <p className='w-full break-all text-ellipsis line-clamp-4 text-neutral-600 text-sm leading-7'>
+            {des}
+          </p>
         </div>
         <div>
           {tag && <Tag tags={tag} />}
