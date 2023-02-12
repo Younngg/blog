bbc42650521245a8ab0dd7fab1fb25f6e2436df7
Jiyeong Jeong <8533283@naver.com>
2023-02-12 18:19:49 +0900

    fix: title 수정

--- data/metadata.ts
+++ data/metadata.ts
@@ -1,7 +1,9 @@
 const metadata = [
   { link: '/', title: '지영의 블로그' },
   { link: '/study', title: '지영의 블로그 | Study' },
+  { link: '/study/[id]', title: '지영의 블로그 | Study' },
   { link: '/daily', title: '지영의 블로그 | Daily' },
+  { link: '/daily/[id]', title: '지영의 블로그 | Daily' },
 ];
 
 export default metadata;
